ca1a9e694cce88171660d78bb9db5588dc306de1
Leonardo <fanger53@gmail.com>
2021-06-09 18:06:04 -0500

    install webpack and config the necesary modules

---
+++ src/Js/game.js
@@ -0,0 +1,2 @@
+import '../assets/style.scss';
+import Phaser from 'phaser';
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/Js/game.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+};
