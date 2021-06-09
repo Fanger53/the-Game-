e1a1f61d6d438c5b68d19e16d45bc4698aa07184
Leonardo <fanger53@gmail.com>
2021-06-09 18:13:39 -0500

    add imgs and sounds files assets

--- src/Js/game.js
+++ src/Js/game.js
@@ -1,2 +1,24 @@
 import '../assets/style.scss';
 import Phaser from 'phaser';
+
+
+const config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 600,
+  parent: 'board-game',
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { x: 0, y: 0 }
+    }
+  },
+  scene: [SceneStart,
+    SceneMain,
+    SceneLast],
+  pixelArt: true,
+  roundPixels: true
+};
+
+const game = new Phaser.Game(config);
+
