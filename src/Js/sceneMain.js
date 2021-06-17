ecdc144384c5a2ad36286a2abfca7d73eb2da185
Leonardo <fanger53@gmail.com>
2021-06-16 19:35:28 -0500

    add enemies

--- src/Js/sceneMain.js
+++ src/Js/sceneMain.js
@@ -1,4 +1,7 @@
 import Player from './entities/player';
+import ChaserShip from './entities/enemy1';
+import GunShip from './entities/enemy2';
+import CarrierShip from './entities/enemy3';
 import sprBg0 from '../assets/img/sprBg0.png';
 import sprBg1 from '../assets/img/sprBg1.png';
 import sprExplosion from '../assets/img/sprExplosion.png';
@@ -50,7 +53,7 @@ this.load.audio('sndLaser', sndLaser);
     this.player = new Player(
       this,
       this.game.config.width * 0.5,
-      this.game.config.height * 0.5,
+      this.game.config.height * 0.6,
       "sprPlayer"
     );
 
@@ -95,6 +98,50 @@ this.load.audio('sndLaser', sndLaser);
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+
+    this.time.addEvent({
+      delay: 1000,
+      callback: function() {
+        var enemy = null;
+
+    if (Phaser.Math.Between(0, 10) >= 3) {
+      enemy = new GunShip(
+        this,
+        Phaser.Math.Between(0, this.game.config.width),
+        0
+      );
+    }
+    else if (Phaser.Math.Between(0, 10) >= 5) {
+      if (this.getEnemiesByType("ChaserShip").length < 5) {
+
+        enemy = new ChaserShip(
+          this,
+          Phaser.Math.Between(0, this.game.config.width),
+          0
+        );
+      }
+    }
+    else {
+      enemy = new CarrierShip(
+        this,
+        Phaser.Math.Between(0, this.game.config.width),
+        0
+      );
+    }
+
+    if (enemy !== null) {
+      enemy.setScale(Phaser.Math.Between(10, 20) * 0.1);
+      this.enemies.add(enemy);
+    }
+      },
+      callbackScope: this,
+      loop: true
+    });
+
   }
 
   update() {
@@ -113,5 +160,16 @@ this.load.audio('sndLaser', sndLaser);
     else if (this.keyD.isDown) {
       this.player.moveRight();
     }
+  }
+
+  getEnemiesByType(type) {
+    var arr = [];
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+      if (enemy.getData("type") == type) {
+        arr.push(enemy);
+      }
+    }
+    return arr;
   }
 }
