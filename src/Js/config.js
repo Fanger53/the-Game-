cf240af0595a276c98b0b759cc99a062901ee3f4
Leonardo <fanger53@gmail.com>
2021-07-23 10:51:28 -0500

    refactoring game.js

---
+++ src/Js/config.js
@@ -0,0 +1,26 @@
+import Phaser from 'phaser';
+import SceneStart from './sceneStart';
+import SceneMain from './sceneMain';
+import SceneLast from './sceneLast';
+import SceneScore from './sceneScore';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 550,
+  parent: 'board-game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  scene: [SceneStart,
+    SceneMain,
+    SceneLast,
+    SceneScore],
+  pixelArt: true,
+  roundPixels: true,
+};
+
+export default config;
--- src/Js/game.js
+++ src/Js/game.js
@@ -1,27 +1,5 @@
 import Phaser from 'phaser';
 import '../assets/style.scss';
-import SceneStart from './sceneStart';
-import SceneMain from './sceneMain';
-import SceneLast from './sceneLast';
-import SceneScore from './sceneScore';
-
-const config = {
-  type: Phaser.AUTO,
-  width: 480,
-  height: 550,
-  parent: 'board-game',
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { x: 0, y: 0 },
-    },
-  },
-  scene: [SceneStart,
-    SceneMain,
-    SceneLast,
-    SceneScore],
-  pixelArt: true,
-  roundPixels: true,
-};
+import config from './config';
 
 window.game = new Phaser.Game(config);
